b4772dc3f0349096d6c3956cc3b3f6d873947571
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2025-09-06 19:40:40 -0400

    Update community and project names in ManualVerify class
    - Changed community from "Spring" to "Apache"
    - Updated project name from "spring-integration" to "dubbo"
    - Set runCommand to false to prevent re-running maven/gradle build

--- src/main/java/com/mockanalyzer/manual/ManualVerify.java
+++ src/main/java/com/mockanalyzer/manual/ManualVerify.java
@@ -7,13 +7,13 @@ import com.mockanalyzer.exporter.MockCloneExporter;
 
 public class ManualVerify {
     public static void main(String[] args) throws Exception {
-        String community = "Spring";
+        String community = "Apache";
 
-        String projectName = "spring-integration";
+        String projectName = "dubbo";
 
         Path projectRoot = Paths.get("C:\\Java_projects\\"+community+"\\" + projectName);
         String outputPath = "C:\\Users\\10590\\OneDrive - stevens.edu\\PHD\\2025 Spring\\mock clone detection\\mock-clone-analyzer\\Example Test File\\" + projectName + " clone.json";
-        boolean runCommand = true; // Set to true if you want to re-run maven/gradle build
+        boolean runCommand = false; // Set to true if you want to re-run maven/gradle build
 
         MockCloneExporter.exportClones(projectRoot, outputPath, runCommand);
 
